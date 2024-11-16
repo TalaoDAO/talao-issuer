2ce3df3646a839680da4d02f09599d4db2318313
Thierry Thevenet <thierry.thevenet@talao.io>
2024-11-16 14:01:14 +0100

    update

--- routes/web_emailpass.py
+++ routes/web_emailpass.py
@@ -62,8 +62,7 @@ def emailpass(mode):
     if request.method == 'GET':
         if request.args.get('format') in['jwt_vc_json', "vc_sd_jwt", "vcsd-jwt"]:
             format = request.args.get('format')
-            if format == "vcsd-jwt":
-                format = "vc_sd_jwt"
+            if format == "vcsd-jwt": format = "vc_sd_jwt"
         else:
             format = 'ldp_vc'
         if not request.args.get('draft') and format == 'ldp_vc':
